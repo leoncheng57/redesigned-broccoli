d64c65a01462b6364b08c911af43ff11d343dfe4
Leon Cheng <57leonardo@gmail.com>
2020-12-02 23:23:45 -0500

    react with webpack initially working

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const js = {
+    test: /\.(js|jsx)$/,
+    exclude: /(node_modules)/,
+    use: {
+        loader: 'babel-loader',
+    }
+}
+
 
 
 module.exports = {
-    entry: './src/index.js',
+    entry: { 
+        index: path.resolve(__dirname, "src", "index.js") 
+    },
+    module: {
+        rules: [js]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, "src", "index.html")
+        })
+    ],
     output: {
         path: path.resolve(__dirname, 'docs'),
         filename: '[name].js'
+    },
+    devServer: {
+        port: 3000,
+        watchContentBase: true,
     }
 }
