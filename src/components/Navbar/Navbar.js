04d39a3e88cbbadb98f94c78d8328701dae77644
Leon Cheng <57leonardo@gmail.com>
2020-12-06 00:03:16 -0500

    implemented clickToCopy for my email!

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,39 +8,90 @@ const NavbarContainer = styled.div`
     color: ${props => props.theme.textColor};
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
     align-items: center;
     height: 80px;
 `;
 
-const NameBox = styled.div`
-    background-color: ${props => props.theme.textColor};
-    color: ${props => props.theme.backgroundPrimary};
-    font-weight: 900;
-    padding: 5px 10px;
-    margin: 0 20px;
-    font: normal normal bold 23px/28px Chivo;
+const LeftItems = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    #NameBox {
+        background-color: ${props => props.theme.textColor};
+        color: ${props => props.theme.backgroundPrimary};
+        font-weight: 900;
+        padding: 5px 10px;
+        margin: 0 20px;
+        font: normal normal bold 23px/28px Chivo;
+    }
+
+    svg {
+        margin: 0 7px;
+    }
+    svg:hover {
+        opacity: 0.6;
+        cursor: pointer;
+    }
+
+    p {
+        font: normal normal normal 16px/23px Overpass;
+        cursor: pointer;
+    }
 `;
 
+
+
+const RightItems = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+
+
 export default function(props) {
+
+    const myEmail = "leonc@mit.edu";
+
+    // Set up an invisible input element so that Click to Copy can work
+    const clickToCopy = () => {
+        const body = document.getElementsByTagName('body')[0];
+        const invisibleInput = document.createElement("input");
+        invisibleInput.setAttribute('value', myEmail);
+        body.appendChild(invisibleInput);
+        invisibleInput.select();
+        document.execCommand("copy");
+        body.removeChild(invisibleInput);
+    }
+
+
     return (
         <ThemeProvider theme={props.theme}>
             <NavbarContainer>
-                <NameBox>
-                    LEON CHENG
-                </NameBox>
-                <GithubIcon fill={props.theme.textColor}/>
-                <LinkedInIcon fill={props.theme.textColor}/>
-                <MailIcon fill={props.theme.textColor}/>
-                leonc@mit.edu
-
-                Top 
-                Education
-                Projects
-                Languages
-                Work
-
-                <MoonIcon fill={props.theme.highlight}/>
-                <TogglePill/>
+                <LeftItems>
+                    <div id="NameBox">
+                        LEON CHENG
+                    </div>
+                    <GithubIcon fill={props.theme.textColor}/>
+                    <LinkedInIcon fill={props.theme.textColor}/>
+                    <MailIcon fill={props.theme.textColor}/>
+                    <p id="myEmail" onClick={clickToCopy}>
+                        {myEmail}
+                    </p>
+                </LeftItems>
+                <RightItems>
+                    <p>Top</p> 
+                    <p>Education</p>
+                    <p>Projects</p>
+                    <p>Languages</p>
+                    <p>Work</p>
+                    <MoonIcon fill={props.theme.highlight}/>
+                    <TogglePill/>
+                </RightItems>
             </NavbarContainer>
         </ThemeProvider>
     )
