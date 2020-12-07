d9933a592d51aa94b17cea677864762566c17d16
Leon Cheng <57leonardo@gmail.com>
2020-12-06 21:24:12 -0500

    first try at responsive display LaptopNav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,10 +2,20 @@ import React from "react";
 import styled, { ThemeProvider } from "styled-components";
 import LaptopNav from "./LaptopNav/LaptopNav";
 
+const LaptopOnly = styled.div`
+    display: none;
+
+    @media ${props => props.theme.device.laptop} {
+        display: block;
+    }
+`;
+
 export default function(props) {
     return (
         <ThemeProvider theme={props.theme}>
-            <LaptopNav theme={props.theme}/>
+            <LaptopOnly>
+                <LaptopNav theme={props.theme}/>
+            </LaptopOnly>
             <div>Mobile Nav</div>
             <div>Mobile Menu</div>
         </ThemeProvider>
